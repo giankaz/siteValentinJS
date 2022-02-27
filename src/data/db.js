ab89162261ae51e1431e68be475d2f62ebbd4f6a
Giankaz <91921192+Giankaz@users.noreply.github.com>
2022-02-27 01:34:06 -0300

    added new photos

--- src/data/db.js
+++ src/data/db.js
@@ -3,7 +3,9 @@ export let dataBase =
         img1 : `A primeira vez que meus pais me viram, eu era bem pequenininho, menor que um Kiwi \uD83E\uDD5D`,
         img2 : `Tão pequenininho, mas já era tão amado \uD83E\uDD70`,
         img3 : `Um pacotinho de amor \uD83D\uDC93`,
-        img4 : `Aqui eu já estava bem grandinho, tinha quase 7 meses. Estava forte e saudável, chutava o dia inteiro \uD83D\uDE20`
+        img4 : `Aqui eu já estava bem grandinho, tinha quase 7 meses. Estava forte e saudável, chutava o dia inteiro \uD83D\uDE20`,
+        img5 : `Eu e meu irmãozinho bat \uD83D\uDC31`,
+        img6 : `Meu irmãozinho Bat e eu \uD83D\uDE3B`
     }
 
 
