22b2da54b44dd8ae1dd53a9d0be09ba6c448749a
Giankaz <91921192+Giankaz@users.noreply.github.com>
2022-03-03 01:42:34 -0300

    changed the functions to a class

--- src/script.js
+++ src/script.js
@@ -4,53 +4,53 @@ import {Visitors} from './models/visits.js'
 const imgStand = document.querySelector('#imgStand')
 const legendDiv = document.querySelector('#legend')
 
+class Main {
 
-let counter = 1
+    static counter = 1
+ 
+    static imgSwapper () {
 
-function imgSwapper () {
-      imgStand.innerHTML = ''
-
-      const img = document.createElement('img')
-      img.src = `./src/imgs/00${counter}.jfif`
-      img.style.animation = 'roll-in-top 0.6s ease-out both'
-      imgStand.appendChild(img)
-
-      legendDiv.innerHTML = ''
+       imgStand.innerHTML = ''
+ 
+       const img = document.createElement('img')
+       img.src = `./src/imgs/00${Main.counter}.jfif`
+       img.style.animation = 'roll-in-top 0.6s ease-out both'
+       imgStand.appendChild(img)
+ 
+       legendDiv.innerHTML = ''
+     
+       legendDiv.style.animation = 'puff-in-center 0.6s cubic-bezier(0.470, 0.000, 0.745, 0.715) both'
+       const p = document.createElement('p')
+       let imgFinder = `img${Main.counter}`
+       p.innerText = dataBase[imgFinder]
+       legendDiv.appendChild(p)
+ 
+         Main.counter++
+     if (Main.counter ===  Object.keys(dataBase).length + 1) {
+         Main.counter = 1
+     }    
+ 
+     setTimeout(() => {
+         legendDiv.style.animation = 'fade-out 0.6s ease-out both'
+         img.style.animation = 'rotate-out-center 0.6s cubic-bezier(0.550, 0.085, 0.680, 0.530) both'
+     }, 5000);
     
-      legendDiv.style.animation = 'puff-in-center 0.6s cubic-bezier(0.470, 0.000, 0.745, 0.715) both'
-      const p = document.createElement('p')
-      let imgFinder = `img${counter}`
-      p.innerText = dataBase[imgFinder]
-      legendDiv.appendChild(p)
-
-      counter++
-    if (counter ===  Object.keys(dataBase).length + 1) {
-        counter = 1
-    }    
-
-    setTimeout(() => {
-        legendDiv.style.animation = 'fade-out 0.6s ease-out both'
-        img.style.animation = 'rotate-out-center 0.6s cubic-bezier(0.550, 0.085, 0.680, 0.530) both'
-    }, 5000);
-   
-}
-
-function loopFunction(delay, callback){
-
-    let loop = function(){
-
-        callback()
-        setTimeout(loop, delay)
-    }
-    loop()
-}
-
-loopFunction(6000, imgSwapper)
-
-
-
-
+     }
+ 
+     static loopFunction(delay, callback){
+ 
+     let loop = function(){
+ 
+         callback()
+         setTimeout(loop, delay)
+     }
+     loop()
+     }
+ }
+ 
 const aside = document.querySelector('aside')
+
+Main.loopFunction(6000, Main.imgSwapper)
 
 document.addEventListener('submit', (event) => {
     event.preventDefault()
